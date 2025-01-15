24d0e3c9bf72ea25ffc12da78fe5f72075150e42
Giuseppe Guerra <giuseppe.guerra@grafana.com>
2025-01-15 17:33:15 +0100

    Publish: Add support for scopes, add --dry-run to publish script (#10)
    * Publish: Add support for scopes, add --dry-run to publish script
    
    * Publish: make scopes required
    
    * Publish action: make scopes required
    
    * CD: Add scopes input and pass it to plugin publish action
    
    * temporarily switch branch
    
    * Make scopes comma-separated
    
    * Revert branch change
    
    * Make scopes optional and default to universal
    
    * temp switch branch
    
    * test new secrets
    
    * fix secret path
    
    * Revert "fix secret path"
    
    This reverts commit 3b57fde15c120e18da71f0b87485d1ad788fa01d.
    
    * Revert "test new secrets"
    
    This reverts commit 34aac34915732db39601eea5edb42edf0c6f8b4b.
    
    * Revert "temp switch branch"
    
    This reverts commit 707334db1dadb3462845c62b28eff6a82290e5c0.

--- actions/plugins/publish/publish/publish.sh
+++ actions/plugins/publish/publish/publish.sh
@@ -2,7 +2,7 @@
 set -e
 
 usage() {
-    echo "Usage: $0 --environment <dev|ops|prod> <plugin_zip_urls...>"
+    echo "Usage: $0 --environment <dev|ops|prod> [--scopes <comma_separated_scopes>] [--dry-run] <plugin_zip_urls...>"
 }
 
 json_obj() {
@@ -10,9 +10,13 @@ json_obj() {
 }
 
 gcs_zip_urls=()
+scopes=''
+dry_run=false
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         --environment) gcom_env=$2; shift 2;;
+        --scopes) scopes=$(echo $2 | jq -Rc 'split(",")'); shift 2;;
+        --dry-run) dry_run=true; shift;;
         --help)
             usage
             exit 0
@@ -40,6 +44,10 @@ if [ -z $gcom_env ]; then
     echo "Environment not provided"
     usage
     exit 1
+fi
+
+if [ -z $scopes ]; then
+    scopes='["universal"]'
 fi
 
 has_iap=false
@@ -94,7 +102,6 @@ for zip_url in $gcs_zip_urls; do
         # os-arch zip
         platform="$os-$arch"
     fi
-    echo $file
 
     # Try to get the .md5 for the zip file
     md5_url="$zip_url.md5"
@@ -127,9 +134,14 @@ json_payload=$(jq -c -n \
     --argjson download "$json_download" \
     --arg url "$GITHUB_SERVER_URL/$GITHUB_REPOSITORY" \
     --arg commit "$sha" \
+    --argjson scopes "$scopes" \
     '$ARGS.named'
 )
 echo $json_payload | jq
+if [ "$dry_run" = true ]; then
+    echo "Dry run enabled, skipping publish"
+    exit 0
+fi
 out=$(
     curl -sSL \
         -X POST \
